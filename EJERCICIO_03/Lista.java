c398d50abe15bde85d3b9fc31f9799b9e08a3983
Jeanpierre-Arteaga <jeanpierre.arteaga@unmsm.edu.pe>
2024-09-26 20:46:09 -0500

    Add files via upload

---
+++ EJERCICIO_03/Lista.java
@@ -0,0 +1,31 @@
+package LABORATORIO_03.EJERCICIO_03;
+
+public class Lista {
+    private Nodo cabeza;
+    public Lista() {
+    cabeza = null;
+    }
+    public void incluir(String nValor) {
+    Nodo aux = new Nodo(nValor);
+    if (cabeza == null) {
+    cabeza = aux;
+    }
+    else {
+    Nodo aux2;
+    aux2=cabeza;
+    while(aux2.darSiguiente() != null) {
+    aux2 = aux2.darSiguiente();
+    }
+    aux2.insertarAContinuacion(aux);
+    }
+    }
+    public void imprimir() {
+    if(cabeza != null) {
+        Nodo aux3 = cabeza;
+while(aux3 != null) {
+System.out.println(aux3.darValor());
+aux3 = aux3.darSiguiente();
+}
+}
+}
+}
---
+++ EJERCICIO_03/Nodo.java
@@ -0,0 +1,19 @@
+package LABORATORIO_03.EJERCICIO_03;
+
+public class Nodo {
+    private String valor;
+    private Nodo siguiente;
+    public Nodo(String cadena) {
+    valor = cadena;
+    siguiente = null;
+    }
+    public String darValor() {
+    return (valor);
+    }
+    public Nodo darSiguiente() {
+    return (siguiente);
+    }
+    public void insertarAContinuacion(Nodo siguienteNodo){
+    siguiente = siguienteNodo;
+    }
+    }
